642e80a4910f94ce5b2b9629c5ecdb855e5bbebe
KaruriCreations <266662126+KaruriCreations@users.noreply.github.com>
2026-07-03 13:42:27 +0300

    added route for gettin 1 item

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ inventory = []
 def get_all_items():
     return jsonify({'inventory': inventory}), 200
 
+@app.route('/inventory/<int:id>', methods=['GET'])
+def get_item():
+    item = next((item for item in inventory if item['id'] == id), None)
+    return jsonify({'item': item}), 200
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
