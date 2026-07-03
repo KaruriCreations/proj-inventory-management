cf3e7da7c6a634e9d697745407d350d3a93c94d9
KaruriCreations <266662126+KaruriCreations@users.noreply.github.com>
2026-07-03 14:46:50 +0300

    created my main menu for the cli

--- cli_frontend.py
+++ cli_frontend.py
@@ -1,5 +1,15 @@
 import requests
 from app import get_all_items, get_item, create_item, update_item, delete_item
 
+def show_menu():
+    print("\n=== Inventory Management System ===")
+    print("1. Show all items")
+    print("2. Show an item")
+    print("3. Create an item")
+    print("4. Update an item")
+    print("5. Delete an item")
+    print("6. Exit")
+    
+
 
 
